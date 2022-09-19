4a3250fcc35f1947fcedb17306836def55a337ad
hajer1098 <hajerboukhari2018@gmail.com>
2022-09-19 10:09:51 +0100

    GET Requests

--- src/index.js
+++ src/index.js
@@ -8,5 +8,21 @@ const PORT=3001;
 app.listen(PORT,()=>console.log(`Running Express Server on Port ${PORT}`));
 
 app.get('/groceries',(req,res)=>{
-    res.send("/groceries")
+    res.send([
+    {
+        item:'milk',
+        quantity:2,
+    },
+    {
+        item:'jus',
+        quantity:1,
+    },
+    {
+        item:'ceral',
+        quantity:2,
+    },
+
+
+
+    ])
 })
